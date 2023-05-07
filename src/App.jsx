605c5e5f0b0b91a61cf3e9f449d5a88e57458128
tsungilin <mt5718214@gmail.com>
2023-05-07 15:32:01 +0800

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react'
 import Info from './info'
 
 function App() {
-  let number = 0
+  // react component在state或props改變時都會重新渲染
+  // state狀態
+  let [number, setNumber] = useState(0)
   const users = [
     { name: "小明", age: 16 },
     { name: "小美", age: 17 },
@@ -12,7 +15,7 @@ function App() {
     alert("你按了button!")
   }
   const buttonHandler2 = (number) => {
-    alert(`加一了！, 現在數值是${++number}`)
+    setNumber(++number)
   }
 
   return (
@@ -26,6 +29,7 @@ function App() {
       <br />
       <button onClick={buttonHandler}>快按我！</button>
       <button onClick={() => { buttonHandler2(number) }}>加1</button>
+      <p>現在數值是{number}</p>
     </div>
   );
 }
