da95a94628bb5a0ea21db25cb7f398149b5105ef
tsungilin <mt5718214@gmail.com>
2023-05-07 13:30:46 +0800

    event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Info from './info'
 
 function App() {
+  let number = 0
   const users = [
     { name: "小明", age: 16 },
     { name: "小美", age: 17 },
     { name: "小華", age: 15 },
   ]
+
+  const buttonHandler = () => {
+    alert("你按了button!")
+  }
+  const buttonHandler2 = (number) => {
+    alert(`加一了！, 現在數值是${++number}`)
+  }
+
   return (
     <div>
       <h1>這是App.js的h1標籤</h1>
@@ -13,6 +22,10 @@ function App() {
       {users.map(user => {
         return <Info name={user.name} age={user.age}/>
       })}
+
+      <br />
+      <button onClick={buttonHandler}>快按我！</button>
+      <button onClick={() => { buttonHandler2(number) }}>加1</button>
     </div>
   );
 }
